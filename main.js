1194d6f070575f49222b80b72b431cb26b5ec4f1
José Angel Cuello <42422488+JCF020@users.noreply.github.com>
2021-02-11 12:07:46 -0400

    Prueba

--- main.js
+++ main.js
@@ -16,6 +16,6 @@ else{
 
 if(diametroRueda > 1.4 && grosorRueda < 0.4 || diametroRueda <= 1.4 && diametroRueda >0.8 || grosorRueda <0.25)
 {
-    alert("El grosor para esta rueda es inferior al recomendado");    
+    alert("El grosor para esta rueda es inferior al recomendados");    
 }
 console.log(diametroRueda);
